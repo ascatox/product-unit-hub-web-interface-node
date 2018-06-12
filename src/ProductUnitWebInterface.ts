d5f315d0f21b7c606afe0a78d9b9a703ba0416e2
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-06-12 14:19:31 +0200

    Event registration works

--- src/ProductUnitWebInterface.ts
+++ src/ProductUnitWebInterface.ts
@@ -1,16 +1,21 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { LedgerClient } from './ledger/LedgerClient';
+import { LedgerClient } from 'node-ledger-client';
 import { logger } from "./Logger";
-
+const config = require('../resources/config-fabric-network.json');
 class ProductUnitWebInterface {
   public express
   private dataLedgerClient: LedgerClient;
 
   constructor() {
-    this.express = express()
-    this.dataLedgerClient = new LedgerClient();
-    this.mountRoutes()
+    this.init();
+  }
+
+  async init() {
+    this.express = express();
+    this.dataLedgerClient = await LedgerClient.init(config);
+    this.mountRoutes();
+
   }
 
   private async getProcessStep(chassisId: string, component: string, subComponent) {
@@ -45,6 +50,17 @@ class ProductUnitWebInterface {
     }
   }
 
+  private async chaincodeEventSubscribe(eventId: string, peerName: string) {
+    return this.dataLedgerClient.registerChaincodeEvent(eventId, peerName, this.onEvent, this.onError);
+  }
+
+  onEvent(name, payload) {
+    console.log('Event arrived!');
+  }
+
+  onError(err) {
+    console.log('Error arrived!');
+  }
   private mountRoutes(): void {
     const router = express.Router();
     router.use(bodyParser.urlencoded({ extended: false }));
@@ -80,7 +96,7 @@ class ProductUnitWebInterface {
     });
 
     router.post('/putData', (req, res) => {
-      const body  = req.body;
+      const body = req.body;
       this.putAnalytics(body.key, body.data).then(data => {
         res.json(data);
       }, error => {
@@ -88,7 +104,12 @@ class ProductUnitWebInterface {
         res.status(error.staus || 500).send(error.message);
       });
     });
-
+   /* this.chaincodeEventSubscribe("EVENT", "peer0.org1.example.com").then((handler) => {
+      console.log('Handler received ' + JSON.stringify(handler));
+    }, (err) => {
+      console.log('Handler received ' + err);
+    });
+    */
 
     this.express.use('/api', router)
   }
